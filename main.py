5bb56a78a2055eab3e85eb716dba3b7e75c177af
Luiz <valuiz1@gmail.com>
2023-03-03 20:20:07 -0300

    Added docstrings to methods

--- main.py
+++ main.py
@@ -17,10 +17,14 @@ import re
 
 
 class InfoGetter:
+    '''Class created for the means of facilitating the search, parsing and data storage of a number
+    of New York Times articles.
+    Initiate the InfoGetter class with its necessary variables, then call the retrieve_info method
+    to start the process.'''
     browser = Selenium()
     request = HTTP()
     excel = Files()
-    browser.auto_close = False
+    browser.auto_close = True
     article_data = {
                 'title': [],
                 'date': [],
@@ -30,12 +34,15 @@ class InfoGetter:
                 'mentions money': []
             }
 
-    def __init__(self, query:str="usa",section_numbers:list=[3,], months_ago:int=1) -> None:
+    def __init__(self, query:str="mexico",section_numbers:list=[3,], months_ago:int=1) -> None:
         self.months_ago = months_ago
         self.section_numbers = section_numbers
         self.query = query
 
     def retrieve_info(self):
+        '''Main method of the InfoGetter class. Initiates the process of gathering information
+        base on the variables that are given. If no variables are given, initiate search based on
+        default values.'''
         browser = self.browser
         section_numbers = self.section_numbers
         query = self.query
@@ -56,9 +63,10 @@ class InfoGetter:
                 browser.click_element(f"xpath:/html/body/div/div[2]/main/div/div[1]/div[2]/div/div/div[2]/div/div/div/ul/li[{sn}]/label/input")
             except ElementNotFound:
                 raise ValueError("Section number does not exist")
+            
+        browser.click_element("class:css-4d08fs")
 
         # self.adjust_date()
-
         self.expand_page()
 
         article_data = self.gather_article_info()
@@ -74,6 +82,9 @@ class InfoGetter:
         # The page needs a moment to update the searches
         sleep(1)
         articles = self.browser.find_elements('class:css-1l4w6pd')
+        if not articles:
+            raise ValueError("Your search returned no results")
+
         for article in articles:
             # Get title
             title = self.get_title(article)
@@ -84,17 +95,10 @@ class InfoGetter:
             # Get description, if exists
             description = self.get_description_if_exist(article)
 
-            # Get image name, if exists
-            try:
-                image = article.find_element('class name', 'css-rq4mmj')
-                image_url = image.get_property('src')
-                image_url = image_url.split("?")[0]
-            except (ElementNotFound, NoSuchElementException):
-                image_url = None
-                image_name = None
-            if image_url is not None:
-                image_name = image_url.split("/")[-1]
-                i = r.download(image_url, target_file=f"images/{image_name}")
+            # Get image name and url, if exists, then download it
+            image_url = self.get_image_url_if_exist(article)
+            image_name = self.get_image_name(image_url)
+            self.donwload_image(image_url, image_name)
 
             # Count how many times the query appear in title and description
             count = self.count_query_appearances(title, description)
@@ -114,11 +118,14 @@ class InfoGetter:
 
         # Add to excel worksheet
         self.save_article_data_to_workbook(self.article_data)
-        breakpoint()
-        self.browser.close_browser()
+        # If its necessary to mandate exactly when to close browser, uncomment below
+        # self.browser.close_browser()
         return self.article_data
     
     def get_dates(self) -> Tuple[str, str]:
+        '''returns two string dates: one of today and one of an amount of months removed
+        from today (months_ago attribute). Returns tuple of start_date and today_date, in 
+        this exact order.'''
         today = date.today()
         today_date = today.strftime("%Y%m%d")
         today = today - relativedelta(months=self.months_ago)
@@ -126,36 +133,58 @@ class InfoGetter:
         return start_date, today_date
     
     def get_title(self, article:WebElement) -> str:
+        '''Receives an article webelement and gets its title element, returning its
+        internal text.'''
         title = article.find_element('class name', 'css-2fgx4k').text
         return title
     
     def get_date(self, article:WebElement) -> str:
+        '''Receives an article webelement and gets its date element, returning its
+        internal text.'''
         date_element = article.find_element('class name', 'css-17ubb9w')
         date = date_element.text
         return date
     
     def get_description_if_exist(self, article:WebElement) -> Union[str, None]:
+        '''Receives an article webelement and checks if there is a description element inside,
+        returning its internal text. If there is no description webelement, returns None.'''
         try:
             description = article.find_element('class name', 'css-16nhkrn').text
         except ElementNotFound:
             description = None
         return description
     
-    # FUNCTION DOES MORE THAN ONE THING! FIX IT!
-    def get_image_name_if_exist(self, article:WebElement) -> Union[str, None]:
+    
+    def get_image_url_if_exist(self, article:WebElement) -> Tuple[Union[str, None]]:
+        '''using an article webelement, retrieves url of an image
+        returns None if there is no image for the article. 
+        The returned variables are structured in this manner: (image_url, image_name)'''
         try:
             image = article.find_element('class name', 'css-rq4mmj')
-            image_url = image.get_property('src')
-            image_url = image_url.split("?")[0]
+            image_src = image.get_property('src')
+            image_url = image_src.split("?")[0]
         except (ElementNotFound, NoSuchElementException):
             image_url = None
+        return image_url
+    
+    def get_image_name(self, image_url:str) -> str:
+        '''Accepts an image source or url, returns the default image filename present in it.
+        If image_url is None, returns None'''
+        if image_url is None:
             image_name = None
-        if image_url is not None:
+        else:
             image_name = image_url.split("/")[-1]
-            self.request.download(image_url, target_file=f"images/{image_name}")
         return image_name
+
+    def donwload_image(self, image_url:str, image_name:str):
+        '''checks if both image_url and image_name are not None, then downloads to the 
+        following directory: ./images/{image_name}'''
+        if image_url and image_name is not None:
+            self.request.download(image_url, target_file=f"images/{image_name}")
     
     def count_query_appearances(self, title:str, description:Union[str, None]) -> int:
+        '''Searches for appearances of query attribute in title and description (if available).
+        Not case sensitive.'''
         nrm_query = self.query.lower()
         nrm_title = title.lower()
         count = nrm_title.count(nrm_query)
@@ -165,6 +194,8 @@ class InfoGetter:
         return count
     
     def check_money_appearance(self, title:str, description:Union[str, None]) -> bool:
+        '''Checks if money appears in title and description(if available) using check_money_on_string
+        method.'''
         if description is not None:
             money_check = self.check_money_on_string(title) or self.check_money_on_string(description)
         else:
@@ -180,6 +211,8 @@ class InfoGetter:
             query_count:int,
             money_check:bool
     ):
+        '''Function that receives all info of an article and appends on all keys of the 
+        article_data attribute.'''
         self.article_data['title'].append(title)
         self.article_data['date'].append(date)
         self.article_data['description'].append(description)
@@ -190,12 +223,19 @@ class InfoGetter:
 
     @property
     def search_terms(self) -> str:
+        '''Property that returns a string that can be used to classify a given search.
+        String returns query, section_numbers and months_ago, separated by a "|" character.'''
         section_numbers = f"{self.section_numbers}".replace("[", "|")
         section_numbers = f"{section_numbers}".replace("]", "|")
         return f"{self.query}{section_numbers}{self.months_ago}"
-    
-    # NEEDS TO BE MORE DYNAMIC, FIX IT!
+
     def save_article_data_to_workbook(self, data:List[Dict[str, Any]]):
+        '''Method that can be used to save all data present in the article_data attribute, if
+        it has any data in it. Saves it in the a directory called data, using the current date 
+        as filename, and the search_terms property as sheet name: "./data/{today}.xlsx"
+        If document and sheet already exists: resets the sheet inside document.
+        If document exists and sheet doesn't: creates a sheet with appropriate name.
+        If neither document or sheet exists: creates a document and sheet with appropriate names.'''
         excel = self.excel
         has_data =  all(self.article_data.values())
         today = date.today().strftime('%Y-%m-%d')
@@ -224,18 +264,24 @@ class InfoGetter:
 
 
     def expand_page(self):
-        showmore_locator = 'xpath:/html/body/div/div[2]/main/div/div[2]/div[2]/div/button'
+        '''Method used to identify the "Show more" button inside the results page and click
+        it until it expands the page to show all articles. A sleep function of 1 second is used
+        inside the loop to prevent the expansion to be too quick, because it can cause article 
+        duplication.'''
+        showmore_locator = 'css:[data-testid="search-show-more-button"]'
         while True:
             
             try:
                 # If expansion is too quick it can duplicate articles
-                sleep(2)
-                self.browser.click_button(showmore_locator)               
+                sleep(1)
+                self.browser.click_button(showmore_locator)  
             except (ElementNotFound, StaleElementReferenceException):
                 break
     
     # OBSOLETE
     def find_and_click_today(self):
+        '''***OBSOLETE!***
+        Suport method used in the adjust_date method to find the appropriate day in the calendar element'''
         today = str(date.today().day)
         days = self.browser.find_elements('xpath:/html/body/div/div[2]/main/div/div[1]/div[2]/div/div/div[1]/div/div/div/div[2]/div/div/div/div/div[2]/div/div[3]/div[1]/div[@class="css-hwsp5p "]')
         for day in days:
@@ -245,6 +291,10 @@ class InfoGetter:
     
     # OBSOLETE
     def adjust_date(self):
+        '''***OBSOLETE!***
+        A method that is used to manually adjust the date in our search page.
+        Made obsolete after figuring out that making that adjustment in the url is quicker
+        and easier.'''
         months_ago = self.months_ago
         # date range
         self.browser.click_button("class:css-p5555t")
@@ -259,9 +309,6 @@ class InfoGetter:
         for _ in range(months_ago):
             self.browser.click_button("class:css-12fzzpb")
         self.find_and_click_today()
-
-        # close date range
-        #self.browser.click_button("class:css-p5555t")
 
     def check_money_on_string(self, string:str) -> bool:
         '''check if inside the article exists a pattern that matches probable ways
